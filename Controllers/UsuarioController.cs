465075890ec650674338a490467ce3896d157dc1
Marcelo_Lima <marcelolima152021@gmail.com>
2022-06-20 15:23:22 -0300

    Pacotes EF Core e Postgres instalados

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using StudApi.Models;
 
 namespace StudApi.Controllers;
 
@@ -6,9 +7,27 @@ namespace StudApi.Controllers;
 [Route("api/[Controller]")]
 public class UsuarioController : ControllerBase
 {
-    [HttpGet]
-    public string Get()
+    public static List<Usuario> Usuarios()
     {
-        return "Ok!";
+        return new List<Usuario>
+        {
+            new Usuario{Id = 1, Nome = "Marco", DataNascimento = new DateTime(2002, 12, 19)}
+        };
+    }
+
+    [HttpGet]
+    public IActionResult Get()
+    {
+
+        return Ok(Usuarios());
+    }
+
+
+    [HttpPost]
+    public IActionResult Post(Usuario usuario)
+    {
+        var usuarios = Usuarios();
+        usuarios.Add(usuario);
+        return Ok(usuarios);
     }
 }
